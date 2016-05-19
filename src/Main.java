0a6a6bd173552d53ea6d7c7c73b839c86793ba29
DAgutierrez <diego.gutierrez684@gmail.com>
2016-05-19 16:31:58 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        User diego = new User();
+
+        String name = diego.getName();
+
+        System.out.println(name);
+
+    }
+}
---
+++ src/User.java
@@ -0,0 +1,22 @@
+/**
+ * Created by diego_gutierrez on 19/5/16.
+ */
+public class User {
+
+    private String name;
+    private int age;
+    private String mail;
+
+    public User() {
+
+        name = "Diego Gutierrez";
+        age = 23;
+        mail = "diego.gutierrez684@gmail.com";
+
+    }
+
+    public String getName () { return name; }
+
+
+
+}
